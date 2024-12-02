7cc037635ee1d3c3b61c0f3b9015e4d8dad02485
Benighter <111303968+Benighter@users.noreply.github.com>
2024-12-02 12:29:02 +0200

    reverted all the changed

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,20 +1,3 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NavigationComponent } from './components/navigation/navigation.component';
-import { AuthService } from './services/auth.service';
-import { CommonModule } from '@angular/common';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  standalone: true,
-  imports: [RouterModule, NavigationComponent, CommonModule]
-})
-export class AppComponent {
-  isLoggedIn = false;
-
-  constructor(private authService: AuthService) {
-    this.isLoggedIn = this.authService.isLoggedIn();
-  }
-}
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,32 +1,10 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
-import { HomeComponent } from './pages/home/home.component';
-import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
-import { PopularMoviesComponent } from './pages/popular-movies/popular-movies.component';
-import { UpcomingMoviesComponent } from './pages/upcoming-movies/upcoming-movies.component';
-import { SearchResultsComponent } from './pages/search-results/search-results.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-// import { DashboardComponent } from './pages/dashboard/dashboard.component';
-
-
-export const routes: Routes = [
-  { path: '', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'movies', component: HomeComponent },
-  { path: 'movie/:id', component: MovieDetailsComponent },
-  { path: 'popular', component: PopularMoviesComponent },
-  { path: 'upcoming', component: UpcomingMoviesComponent },
-  { path: 'search', component: SearchResultsComponent },
-  // Default route
-  // { path: 'dashboard', component: DashboardComponent }, // Protected route
-];
-
 
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+
     exports: [RouterModule],
 })
 export class AppRoutingModule {}
