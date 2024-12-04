36765302ccea7b42d2c222934e84d02fcb8538ef
216037150 <siyabongazungu95@gmail.com>
2024-12-04 14:47:15 +0200

    created navbar and top up button commponents

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TopupbuttonComponent } from './components/topupbutton/topupbutton.component';
 
 @NgModule({
   imports: [BrowserModule],
   providers: [],
   schemas: [],
+  declarations: [
+    NavbarComponent,
+    TopupbuttonComponent
+  ],
 })
 export class AppModule {}
---
+++ client/src/app/components/topupbutton/topupbutton.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topupbutton',
+  templateUrl: './topupbutton.component.html',
+  styleUrls: ['./topupbutton.component.css']
+})
+export class TopupbuttonComponent {
+
+}
