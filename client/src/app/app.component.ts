c67e3a86ebac9c83c92734f429377189c86c69d8
senamilemtshali <mtshalisenamile@gmail.com>
2024-12-06 11:16:55 +0200

    added bcript password

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { RouterModule, RouterOutlet } from '@angular/router';
-import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet, Router, NavigationEnd } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { AuthService } from './services/auth.service';
 import { CommonModule } from '@angular/common';
 import { TopupbuttonComponent } from './components/topupbutton/topupbutton.component';
+import { filter } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +14,35 @@ import { TopupbuttonComponent } from './components/topupbutton/topupbutton.compo
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   isLoggedIn = false;
+  isAuthRoute = false;
+  private routerSubscription?: Subscription;
+  private authSubscription?: Subscription;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
     this.isLoggedIn = this.authService.isLoggedIn();
+  }
+
+  ngOnInit() {
+    this.routerSubscription = this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const url = event.url;
+        this.isAuthRoute = url.includes('/login') || url.includes('/register');
+      }
+    });
+
+    this.authSubscription = this.authService.isLoggedIn$.subscribe(
+      (loggedIn: boolean) => {
+        this.isLoggedIn = loggedIn;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.routerSubscription?.unsubscribe();
+    this.authSubscription?.unsubscribe();
   }
 }
